e4c7343170167dedb966b5e290f8d84b77d2340b
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2026-01-24 15:55:25 -0500

    upt's

---
+++ Labs/GPT/Book/Book.java
@@ -0,0 +1,9 @@
+public class Book {
+    private String title;
+    public Book(String title){
+        this.title=title;
+    }
+    public String getTitle(){
+        return title;
+    }
+}
---
+++ Labs/GPT/Book/main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Book javaBook = new Book("Java Basics");
+        System.out.println(javaBook.getTitle());
+    }
+}
