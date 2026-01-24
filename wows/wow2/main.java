ceefa23e78640e53f1ac3b971fe46b61eaf770cd
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2026-01-23 23:15:13 -0500

    upt

---
+++ wows/wow2/main.java
@@ -0,0 +1,19 @@
+public class main {
+    class Dog{
+        String color;
+        int age;
+        boolean isNice;
+        Dog(String color, int age, boolean isNice){
+            this.age=age;
+            this.color=color;
+            this.isNice=isNice;
+        }
+
+
+
+    }
+
+    public static void main(String[] arg){
+
+    }
+}
