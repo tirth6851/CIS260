12eb322070404686ab5011977fd8268569e4a57f
tirth6851 <130498257+tirth6851@users.noreply.github.com>
2026-01-29 13:58:25 -0500

    ERA upt

--- wows/ERA/eraTP1.java
+++ wows/ERA/eraTP1.java
@@ -1,19 +1,20 @@
 /*
 * Name: Tirth Patel
 * ID: 2919808
-*
 */
 
 
 
 import java.util.ArrayList;
+//import java.util.LinkedList;
 import java.util.Scanner;
-
+import java.util.Collections;
 public class eraTP1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         ArrayList<Integer> listNum = new ArrayList<>();
+        //LinkedList<String> listNum = new LinkedList<>();
         listNum.add(1);
         listNum.add(2);
         listNum.add(3);
@@ -31,6 +32,13 @@ public class eraTP1 {
 
         System.out.println("after input");
         for (int num : listNum) {
+            System.out.println(num);
+        }
+
+        /*SORTING*/
+        System.out.println("Sorted list");
+        Collections.sort(listNum);
+        for(int num:listNum){
             System.out.println(num);
         }
     }
